654ab00ed45166465804ef6d476d4eb4e29f0dbf
NightWalker23 <taptnw@gmail.com>
2017-10-14 21:10:15 +0200

    Perceptron bardziej uniwersalny

--- ProjektNr1/src/perceptron/Main.java
+++ ProjektNr1/src/perceptron/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main ( String[] args ) {
 
-        Perceptron perc = new Perceptron();
+        int number_of_inputs = 3;
+        Perceptron perc = new Perceptron( number_of_inputs );
 
-        int n = 3; //ilość powtórzeń uczenia się
+        int n = 6; //ilość powtórzeń uczenia się
         int r = 4; //rozmiar tablic danych wejściowych
         double learning_rate = 0.1; //krok uczenia się
 
@@ -23,16 +24,19 @@ public class Main {
         //uczenie perceptronu
         for ( int j = 0; j < n; j++ ) {
             for ( int i = 0; i < r; i++ ) {
-                perc.learn( x0, x1[i], x2[i], y[i], learning_rate );
+                perc.learn( new int[] { x0, x1[i], x2[i] }, y[i], learning_rate );
             }
         }
 
-        perc.showW(); //wyświetlenie wag
+        System.out.println( "WAGI:" );
+        for ( int i = 0; i < number_of_inputs; i++ )
+            System.out.println( "w" + i + " = " + perc.getW( i ) );
+        System.out.println();
 
         //testowanie perceptronu
-        System.out.println( perc.process( x0, 0, 0 ) );
-        System.out.println( perc.process( x0, 0, 1 ) );
-        System.out.println( perc.process( x0, 1, 0 ) );
-        System.out.println( perc.process( x0, 1, 1 ) );
+        System.out.println( perc.process( new int[] { x0, 0, 0 } ) );
+        System.out.println( perc.process( new int[] { x0, 0, 1 } ) );
+        System.out.println( perc.process( new int[] { x0, 1, 0 } ) );
+        System.out.println( perc.process( new int[] { x0, 1, 1 } ) );
     }
 }
